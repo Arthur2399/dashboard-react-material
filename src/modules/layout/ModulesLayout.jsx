ab3591928e2a6dbed269b2c3fda68ce4e49cad76
Arthur Chavez <arthurchavez2399@gmail.com>
2023-03-03 13:00:38 -0500

    [UPDATE] validaciones en el login

---
+++ src/modules/layout/ModulesLayout.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/system";
+
+const drawerWith = 280;
+
+export const ModulesLayout = ({children}) => {
+  return (
+    <Box xs={{display:'flex'}}>
+        
+    </Box>
+  )
+}
