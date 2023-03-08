69249224ece19789561bc4d67079ff2b401a1ce2
Arthur Chavez <arthurchavez2399@gmail.com>
2023-03-08 12:02:38 -0500

    [UPDATE] Agregando iconos

--- src/modules/components/SideBarItem.jsx
+++ src/modules/components/SideBarItem.jsx
@@ -1,87 +1,31 @@
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import TreeView from '@mui/lab/TreeView';
 import TreeItem, { treeItemClasses } from '@mui/lab/TreeItem';
 import Typography from '@mui/material/Typography';
-import MailIcon from '@mui/icons-material/Mail';
+
+/* ICONS FOR MODULES */
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import SchoolIcon from '@mui/icons-material/School';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import ComputerIcon from '@mui/icons-material/Computer';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+
 import DeleteIcon from '@mui/icons-material/Delete';
-import Label from '@mui/icons-material/Label';
-import InfoIcon from '@mui/icons-material/Info';
-import ForumIcon from '@mui/icons-material/Forum';
-import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import { useSelector } from 'react-redux';
-
-/* const menuItems = [
-  {
-    id: '1',
-    text: 'All Mail',
-    icon: MailIcon,
-  },
-  {
-    id: '2',
-    text: 'Trash',
-    icon: DeleteIcon,
-  },
-  {
-    id: '3',
-    text: 'Categories',
-    icon: Label,
-    children: [
-      {
-        id: '6',
-        text: 'Updates',
-        icon: InfoIcon,
-        info: '2,294',
-        color: '#e3742f',
-        bgColor: '#fcefe3',
-        children: [
-          {
-            id: '9',
-            text: 'Hola',
-            icon: InfoIcon,
-            info: '2,294',
-            color: '#e3742f',
-            bgColor: '#fcefe3',
-            children: [
-              {
-                id: '10',
-                text: 'JEJE',
-                icon: InfoIcon,
-                info: '2,294',
-                color: '#e3742f',
-                bgColor: '#fcefe3',
-              },]
-          },
-        ]
-      },
-      {
-        id: '7',
-        text: 'Forums',
-        icon: ForumIcon,
-        info: '3,566',
-        color: '#a250f5',
-        bgColor: '#f3e8fd',
-      },
-      {
-        id: '8',
-        text: 'Promotions',
-        icon: LocalOfferIcon,
-        info: '733',
-        color: '#3c8039',
-        bgColor: '#e6f4ea',
-      },
-    ],
-  },
-  {
-    id: '4',
-    text: 'History',
-    icon: Label,
-  },
-]; */
-
 
 
 const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
@@ -114,6 +58,118 @@ const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
     },
   },
 }));
+
+
+
+const example = [
+  {
+    id: 1,
+    text: 'Punto de venta',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: PointOfSaleIcon,
+  },
+  {
+    id:2 ,
+    text: 'Educativo',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: SchoolIcon,
+  },
+  {
+    id:3 ,
+    text: 'Cuentas por pagar',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: PaymentsIcon,
+  },
+  {
+    id:4 ,
+    text: 'Cuentas por cobrar',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: ReceiptIcon,
+  },
+  {
+    id:5 ,
+    text: 'Contabilidad',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: AccountBalanceWalletIcon,
+  },
+  {
+    id:6 ,
+    text: 'Tesoreria',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: MonetizationOnIcon,
+  },
+  {
+    id:7 ,
+    text: 'SRI',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: AccountBalanceIcon,
+  },
+  {
+    id:8 ,
+    text: 'Inventarios',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: InventoryIcon,
+  },
+  {
+    id:9 ,
+    text: 'Activos fijos',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: ComputerIcon,
+  },
+  {
+    id:10 ,
+    text: 'Compras',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: ShoppingCartIcon,
+  },
+  {
+    id:11 ,
+    text: 'Gerencial',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: ApartmentIcon,
+  },
+  {
+    id:12 ,
+    text: 'Administración',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: AdminPanelSettingsIcon,
+  },
+  {
+    id:12 ,
+    text: 'Parametrización',
+    ruute: '/',
+    color: '',
+    textInfo: '10',
+    labelIcon: SettingsSuggestIcon,
+  },
+  
+]
+
+
 
 function StyledTreeItem(props) {
   const {
@@ -158,24 +214,34 @@ StyledTreeItem.propTypes = {
 
 export const SideBarItem = () => {
 
-const {modules} = useSelector(state=> state.menu);
+  const { modules } = useSelector(state => state.menu);
 
   const renderMenuItem = (item) => {
-    const { id,nombre,hijos } = item;
-    if (hijos) {
+    const {
+      id,
+      text,
+      color,
+      textInfo,
+      labelIcon,
+      children,
+    } = item;
+
+    if (children) {
       return (
         <StyledTreeItem
           key={id}
-          nodeId={id}
-          labelText={nombre}
-          /* labelIcon={LabelIcon} */
+          nodeId={id.toString()}
+          color={color}
+          labelText={text}
+          labelInfo={textInfo}
+          labelIcon={labelIcon}
         >
-          {hijos.map((childItem) => renderMenuItem(childItem))}
+          {children.map((childItem) => renderMenuItem(childItem))}
         </StyledTreeItem>
       );
     } else {
       return (
-        <StyledTreeItem key={id} nodeId={id} labelText={nombre} /* labelIcon={LabelIcon} */ />
+        <StyledTreeItem key={id} nodeId={id.toString()} labelText={text} labelIcon={labelIcon} color={color} />
       );
     }
   };
@@ -188,7 +254,7 @@ const {modules} = useSelector(state=> state.menu);
       defaultEndIcon={<div style={{ width: 24 }} />}
       sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
     >
-      {modules.payload.map((item) => renderMenuItem(item))}
+      {example.map((item) => renderMenuItem(item))}
     </TreeView>
   );
 };
