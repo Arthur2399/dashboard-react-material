84e0293bfee18e6a786035bd9ea932c0f925c7b9
Arthur Chavez <arthurchavez2399@gmail.com>
2023-03-20 11:30:12 -0500

    [UPDATE] Change od directory AfterLogin

--- src/modules/routes/ModulesRoutes.jsx
+++ src/modules/routes/ModulesRoutes.jsx
@@ -1,20 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { useCheckAuth } from "../../hooks/useCheckAuth";
-import { AfterLogin } from "../pages/AfterLogin";
-import { ModulesPages } from "../pages/ModulesPages"
+import { ModulesPages } from "../pages"
 
 export const ModulesRoutes = () => {
 
-  const { satatusCompany } = useCheckAuth();
-
   return (
     <Routes>
-      {
-        (satatusCompany === 'no-selected')
-          ? <Route path="/after-login/*" element={<AfterLogin />} />
-          : <Route path="/*" element={<ModulesPages />} />
-      }
-      <Route path="/*" element={<Navigate to="/after-login" />} />
+      <Route path="/" element={<ModulesPages />} />
+      <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -12,6 +12,15 @@ export const AppRouter = () => {
         return <CheckingAuth />
     }
 
+
+    /* NOTA
+        Aqui se deben hacer las validaciones de multiempresa
+    */
+
+    /* const { satatusCompany } = useCheckAuth(); */
+    {/* <Route path="/after-login/*" element={<AfterLogin />} /> */ }
+
+
     return (
         <Routes>
             {
