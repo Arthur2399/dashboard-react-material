606923a9e066f725abe65ee8dc8be481abbf8de5
arthur2399 <arthurchavez2399@gmail.com>
2023-03-23 18:08:02 -0500

    [ADD] Encriptacion de informacion

--- src/store/modules/ui/company/thunks.js
+++ src/store/modules/ui/company/thunks.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import CryptoJS from 'crypto-js';
 import { companyData } from "../../../../data/ui/companyData";
 import { gettingCompanies, loadingCompanies, selectCompany, unselectedCompany } from "./companyInfoSlice";
 
@@ -22,28 +23,64 @@ export const startGetCompany = () => {
         const { token } = getState().auth;
 
         //TODO Realiza la peticción para traer la empresa - NOTA: Poner en un Try - Catch
-        //const { data } = await axios.get(`${config.apiUrl}example/endpoint/companies`, { headers: { Authorization: token } })
+            //const { data } = await axios.get(`${config.apiUrl}example/endpoint/companies`, { headers: { Authorization: token } })
         dispatch(gettingCompanies(companyData));
 
         //Validación seleccion de empresa
+
+        /* NOTA
+            Si el valor de la peticion tiene un listas de objetos superior a uno hara el despacho de
+            unselectedComany() que cambiará el estado a 'no-selected' por el contrario si es menor a 1
+            despachará selectCompany() cambiando el estado a selected.
+        
+        */
         if (companyData.length > 1) return dispatch(unselectedCompany());
 
+        //Desfracmetación y aislamiento del atributo fiscal_exercise
         const { fiscal_exercise, ...newCompanyData } = companyData[0];
 
-        dispatch(selectCompany({ ...newCompanyData, fiscal_exercise: fiscal_exercise[0] }));
+        //Reconstrucción del objeto con el valor de fiscal_exercise en la posición [0]
+        const companySelected = { ...newCompanyData, fiscal_exercise: fiscal_exercise[0] }
+
+        //Encriptación de la información
+        const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(companySelected), 'uva').toString();
+
+        //Guardar la información de la empresa seleccionada en el localStorage del navegador
+        localStorage.setItem("company", encryptedData);
+
+        //Seteo de la informacion en state currentCompany de companyInfoSlice.js
+        dispatch(selectCompany(companySelected));
     }
 }
 
 export const startSelectionCompany = ({ id_company, id_fiscal_exercise }) => {
     return async (dispatch) => {
 
+        /* NOTA
+            De acuerdo a la selección del usuario en el AfterLogin.jsx se hara una búsqueda
+            por id las empresa y ejercicio fiscal, luego se guardará esa informacion en el 
+            currentCompany del companyInfoSlice y en el LocalStorge del navegador.
+        */
+
+        //Busqueda por id entre la lista de empresas
         let onlyCompany = companyData.find(obj => obj.id === id_company)
+
+        //Busqueda por id entre la lista de ejercicio fiscal de la empresa.
         const selectFiscalExercise = onlyCompany.fiscal_exercise.find(obj => obj.id === id_fiscal_exercise)
 
-        const { fiscal_exercise, ...newOnlyCompany } = onlyCompany; // crear una copia de onlyCompany sin la propiedad fiscal_exercise
+        //Aislamiento del atributo fiscal_exercise
+        const { fiscal_exercise, ...newOnlyCompany } = onlyCompany; 
 
-        const selectedCompany = { ...newOnlyCompany, fiscal_exercise: selectFiscalExercise }; // imprimir la nueva copia del objeto con la propiedad actualizada
+        //Construcción del objeto remplazando las lista de fiscal_exercise por el valor seleccionado
+        const selectedCompany = { ...newOnlyCompany, fiscal_exercise: selectFiscalExercise }; 
 
+        //Encriptación de la información
+        const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(selectedCompany), 'uva').toString();
+
+        //Guardar la información de la empresa seleccionada en el localStorage del navegador
+        localStorage.setItem("company", encryptedData);
+        
+        //Seteo de la informacion en state currentCompany de companyInfoSlice.js
         dispatch(selectCompany(selectedCompany))
     }
 }
