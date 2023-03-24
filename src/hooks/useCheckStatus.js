3105335711c5dbe635f1deb01455db60241d33e0
arthur2399 <arthurchavez2399@gmail.com>
2023-03-24 09:39:10 -0500

    [UPDATE] Documentacion del codigo

--- src/hooks/useCheckStatus.js
+++ src/hooks/useCheckStatus.js
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios';
 import CryptoJS from 'crypto-js';
 
-import axios from 'axios';
 
 import { login, logout } from '../store/auth/authSlice';
 import { userData } from '../data/auth/userData';
-import config from '../config';
-import { gettingCompanies, selectCompany, setCompanies, unselectedCompany } from '../store/modules/ui/company/companyInfoSlice';
 import { companyData } from '../data/ui/companyData';
+import { selectCompany, setCompanies, unselectedCompany } from '../store/modules/ui/company/companyInfoSlice';
+import config from '../config';
 
 
 export const useCheckStatus = () => {
@@ -28,6 +28,9 @@ export const useCheckStatus = () => {
             2. Realizar un petición al API y verificar si dicho token es valido.
             3. Si es válido volver enviar los datos del usuario a authSlice rectificando el estado 'authenticated'
             4. Si el token no es válido llama el reducer logout() borrando datos del sessionStorage.
+            5. Verificar si existe hash de encriptación de empresa
+            6. Pedir que seleccione empresa en caso de no existir
+            7. Volver a guardar empresa seleccionada en localStorage encryptado
     */
 
     useEffect(() => {
@@ -42,21 +45,36 @@ export const useCheckStatus = () => {
             */
             if (!token) return dispatch(logout());
             //TODO El multicompany lo debe enviar el API
-            /* const { data } = await axios.get(`${config.apiUrl}/usuarios/api-token-auth/verify`, { headers: { Authorization: token } })
-            dispatch(login({ ...data, multicompany: true })) */
+                /* const { data } = await axios.get(`${config.apiUrl}/usuarios/api-token-auth/verify`, { headers: { Authorization: token } })
+                dispatch(login({ ...data, multicompany: true })) */
             dispatch(login(userData))
 
 
+            /* Nota
+                En caso de no existir compania en el localStorage debe hacer despacho del metodo 
+                unSelectedCompany para selecciones un empresa, caso contrario de volvera a enviar 
+                la información registrada.
+            */
+
+
+            // Valida si existe company en localStorage
             if (!company) return dispatch(unselectedCompany());
+
+            // Setear de nuevo todas las companias
+            //TODO Esto debe enviarse desde el API 
             dispatch(setCompanies(companyData));
+
+            // Desecripta la informacion de compania
             const decryptedData = CryptoJS.AES.decrypt(company, 'uva').toString(CryptoJS.enc.Utf8);
+
+            // Transforma en JSON
             const dataCompany = JSON.parse(decryptedData);
+
+            // Setea la informacion como empresa seleccionada
             dispatch(selectCompany(dataCompany));
 
         }
-
         verifyCredentials();
-
 
     }, [])
 
