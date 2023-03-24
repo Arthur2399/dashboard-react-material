852020732319c5da4faa58b0f89ba793741fb90b
arthur2399 <arthurchavez2399@gmail.com>
2023-03-24 11:30:14 -0500

    [UPDATE] Dashboard

--- src/modules/components/SideBarItem.jsx
+++ src/modules/components/SideBarItem.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const SideBarItem = () => {
-  return (
-    <div>
+  const [menuItems, setMenuItems] = useState([
+    {
+      id: 1,
+      label: 'Item 1',
+      children: [
+        {
+          id: 2,
+          label: 'Subitem 1.1',
+          children: [
+            { id: 3, label: 'Subsubitem 1.1.1' },
+            { id: 4, label: 'Subsubitem 1.1.2' },
+          ],
+        },
+        { id: 5, label: 'Subitem 1.2' },
+      ],
+    },
+    { id: 6, label: 'Item 2' },
+    { id: 7, label: 'Item 3' },
+  ]);
 
-    </div>
-  )
+  const renderMenuItem = (menuItem) => {
+    return (
+      <li key={menuItem.id}>
+        <div className="menu-item">{menuItem.label}</div>
+        {menuItem.children && (
+          <ul className="menu-list">
+            {menuItem.children.map((child) => renderMenuItem(child))}
+          </ul>
+        )}
+      </li>
+    );
+  };
+
+  return (
+    <ul className="menu-tree">{menuItems.map((item) => renderMenuItem(item))}</ul>
+  );
 }
