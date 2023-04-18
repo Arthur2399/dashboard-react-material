49b63d9cfeb69d219276fe0d527290a18c428117
arthur2399 <arthurchavez2399@gmail.com>
2023-04-18 13:04:21 -0500

    [UPDATE] AppTheme

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
-import { blueTheme } from './blueTheme';
+import { theme } from './theme';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -8,7 +8,7 @@ import '@fontsource/roboto/700.css';
 
 export const AppTheme = ({ children }) => {
     return (
-        <ThemeProvider theme={blueTheme}>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
             {children}
         </ThemeProvider>
