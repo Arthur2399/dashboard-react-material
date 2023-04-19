ff5f3cb99ef76f5faf19aa45f1e0f63e2307812b
arthur2399 <arthurchavez2399@gmail.com>
2023-04-19 15:53:42 -0500

    [ADD] Material UI

--- src/modules/layout/ModulesLayout.jsx
+++ src/modules/layout/ModulesLayout.jsx
@@ -1,14 +1,15 @@
-import {SideBar} from '../components/SideBar';
+import { Box } from '@mui/material';
+import { SideBar } from '../components/SideBar';
 import { TopBar } from '../components/TopBar';
 
 export const ModulesLayout = ({ children }) => {
-    return (
-    <div className="app">
-      <SideBar/>
-      <main className="content">
+  return (
+    <Box display="flex" position="relative" width="100%">
+      <SideBar />
+      <Box component='main' width="100%">
         <TopBar />
         {children}
-      </main>
-    </div>
+      </Box>
+    </Box >
   )
 }
