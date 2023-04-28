0c96ddb07d1e5e3e2028f044b39ee4a07d1cdae1
arthur2399 <arthurchavez2399@gmail.com>
2023-04-28 11:33:51 -0500

    [UPDATE]

--- src/data/modules/security/mockDataSecurity.js
+++ src/data/modules/security/mockDataSecurity.js
@@ -17,18 +17,6 @@ export const userData = [
     },
 ]
 
-export const comunidadesCbx = [
-    { value: 1, label: 'Cofavi - Quito norte' },
-    { value: 2, label: 'Iñaquito - Quito centro' },
-    { value: 3, label: 'Quitumbe - Quito sur' },
-];
-
-export const rolesCbx = [
-    { value: 1, label: 'Usuario estandar' },
-    { value: 2, label: 'Unidad policial' },
-    { value: 3, label: 'Presidente' },
-    { value: 4, label: 'Vicepresidente' },
-];
 
 export const communityData =[
     {
@@ -52,4 +40,36 @@ export const communityData =[
         city:"Quito",
         address: "Rircardo Jaramillo y Salomon Cardenas",
     },
+]
+
+
+export const comunidadesCbx = [
+    { value: 1, label: 'Cofavi - Quito norte' },
+    { value: 2, label: 'Iñaquito - Quito centro' },
+    { value: 3, label: 'Quitumbe - Quito sur' },
+];
+
+export const rolesCbx = [
+    { value: 1, label: 'Usuario estandar' },
+    { value: 2, label: 'Unidad policial' },
+    { value: 3, label: 'Presidente' },
+    { value: 4, label: 'Vicepresidente' },
+];
+
+export const provineCbx =[
+    {value :1, label:"Pichincha"},
+    {value :2, label:"Guayas"},
+    {value :3, label:"Tungurahua"},
+]
+
+export const cityCbx = [
+    {value:1 , label:"Quito"},
+    {value:2 , label:"Guayaquil"},
+    {value:3 , label:"Ambato"},
+]
+
+export const userCbx =[
+    {value:1 , label:"Jordi Fiallos"},
+    {value:2 , label:"Miguel Pino"},
+    {value:3 , label:"Arthur Chavez"},
 ]
--- src/modules/modules/security/pages/CommunityForm.jsx
+++ src/modules/modules/security/pages/CommunityForm.jsx
@@ -1,6 +1,200 @@
+import { Alert, AlertTitle, Autocomplete, Box, Button, TextField, useMediaQuery } from "@mui/material";
+import { Header } from "../../components";
+import { Field, Form, Formik } from "formik";
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import SaveIcon from '@mui/icons-material/Save';
+import { useState } from "react";
+import { cityCbx, provineCbx, userCbx } from "../../../../data/modules/security/mockDataSecurity";
 
 export const CommunityForm = () => {
-  return (
-    <div>CommunityForm</div>
-  )
+
+    const [alertMessage, setAlertMessage] = useState(false);
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+
+
+    return (
+        <Box className="animate__animated animate__fadeIn">
+            <Header title="Crear comunidad" subtitle="Crea la comunidad para organizar a los usuarios." />
+            <Formik
+                initialValues={initialValues}
+                /* validationSchema={validationSchema} */
+                onSubmit={(values) => {
+                    console.log(JSON.stringify(values))
+                }}
+            >
+                {({ values, errors, touched, setFieldValue, setFieldTouched, resetForm }) => (
+                    <Form>
+                        <Box
+                            display="grid"
+                            gap="30px"
+                            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                            sx={{
+                                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                            }}
+                        >
+
+                            {/* NOMBRE DE COMUNIDAD */}
+                            <Field
+                                as={TextField}
+                                type="text"
+                                fullWidth
+                                variant="filled"
+                                label="Nombre de comunidad"
+                                placeholder="Ingrese el nombre de la comunidad"
+                                name="communityName"
+                                error={errors.communityName && touched.communityName}
+                                helperText={errors.communityName && touched.communityName && errors.communityName}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+
+                            {/* PROVINCIA */}
+                            <Autocomplete
+                                options={provineCbx}
+                                getOptionLabel={(option) => option.label}
+                                value={provineCbx.find((option) => option.value === values.province) || null}
+                                onBlur={() => setFieldTouched('province', true)}
+                                onChange={(event, newValue) => {
+                                    setFieldValue('province', newValue ? newValue.value : null);
+                                }}
+                                sx={{ gridColumn: "span 2" }}
+                                renderInput={(params) =>
+                                    <TextField {...params}
+                                        label="Provincia"
+                                        placeholder="Busque y escoja una provincia"
+                                        name="province"
+                                        error={errors.province && touched.province}
+                                        helperText={errors.province && touched.province && errors.province}
+                                        variant="filled" />}
+                            />
+
+                            {/* CIUDAD */}
+                            <Autocomplete
+                                options={cityCbx}
+                                getOptionLabel={(option) => option.label}
+                                value={cityCbx.find((option) => option.value === values.city) || null}
+                                onBlur={() => setFieldTouched('city', true)}
+                                onChange={(event, newValue) => {
+                                    setFieldValue('city', newValue ? newValue.value : null);
+                                }}
+                                sx={{ gridColumn: "span 2" }}
+                                renderInput={(params) =>
+                                    <TextField {...params}
+                                        label="Ciudad"
+                                        placeholder="Busque y escoja una ciudad"
+                                        name="city"
+                                        error={errors.city && touched.city}
+                                        helperText={errors.city && touched.city && errors.city}
+                                        variant="filled" />}
+                            />
+
+                            {/* DIRECCIÓN */}
+                            <Field
+                                as={TextField}
+                                type="text"
+                                fullWidth
+                                variant="filled"
+                                label="Dirección"
+                                placeholder="Ingrese la dirección de la comunidad"
+                                name="address"
+                                error={errors.address && touched.address}
+                                helperText={errors.address && touched.address && errors.address}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+
+                            {/* MENSAJE VERDE */}
+                            <Field
+                                as={TextField}
+                                type="text"
+                                fullWidth
+                                multiline
+                                variant="filled"
+                                label="Mensaje de prioridad baja"
+                                minRows={5}
+                                placeholder="Ingrese mensaje de prioridad baja"
+                                name="messageGreen"
+                                error={errors.messageGreen && touched.messageGreen}
+                                helperText={errors.messageGreen && touched.messageGreen && errors.messageGreen}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+
+                            {/* MENSAJE AMARILLO*/}
+                            <Field
+                                as={TextField}
+                                type="text"
+                                fullWidth
+                                multiline
+                                variant="filled"
+                                label="Mensaje de prioridad media"
+                                minRows={5}
+                                placeholder="Ingrese mensaje de prioridad media"
+                                name="messageYellow"
+                                error={errors.messageYellow && touched.messageYellow}
+                                helperText={errors.messageYellow && touched.messageYellow && errors.messageYellow}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+
+                            {/* MENSAJE Rojo */}
+                            <Field
+                                as={TextField}
+                                type="text"
+                                fullWidth
+                                multiline
+                                variant="filled"
+                                label="Mensaje de prioridad alta"
+                                minRows={5}
+                                placeholder="Ingrese mensaje de prioridad alta"
+                                name="messageRed"
+                                error={errors.messageRed && touched.messageRed}
+                                helperText={errors.messageRed && touched.messageRed && errors.messageRed}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+
+                        </Box>
+                        <Box display="flex" justifyContent="end" mt="20px">
+                            <Button type="button" title="Cancelar" color="primary" variant="outlined" sx={{ mr: 1 }}>
+                                <DeleteIcon />
+                            </Button>
+                            <Button type="button" title="Reiniciar" color="primary" variant="outlined" sx={{ mr: 1 }}
+                                onClick={resetForm}
+                            >
+                                <RestartAltIcon />
+                            </Button>
+                            <Button type="submit" title="Crear" color="primary" variant="contained" sx={{ mr: 1 }}>
+                                <SaveIcon sx={{ mr: 1 }} />
+                                Guardar
+                            </Button>
+                        </Box>
+                    </Form>
+                )}
+            </Formik>
+
+            <Alert
+                variant="filled"
+                severity="error"
+                className='animate__animated animate__backInRight'
+                /* onClick={() => { setAlertMessage(!alertMessage) }} */
+                sx={alertMessage === false ? { display: "none" }
+                    : {
+                        position: "fixed",
+                        top: "70px",
+                        right: "10px"
+                    }}>
+                <AlertTitle>¡Error!</AlertTitle>
+                Hubo un problema en el <strong>servidor.</strong>
+            </Alert>
+        </Box>
+    )
+}
+
+
+const initialValues = {
+    communityName: "",
+    province: null,
+    city: null,
+    address: "",
+    messageGreen: "",
+    messageYellow: "",
+    messageRed: "",
 }
