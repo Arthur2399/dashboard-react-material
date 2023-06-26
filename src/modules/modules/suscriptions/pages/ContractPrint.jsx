1c73794d2390b6671f3df7c862bad4cf0b8c11f7
Arthur2399 <arthurchavez2399@gmail.com>
2023-06-26 14:10:15 -0500

    [

--- src/modules/modules/suscriptions/pages/ContractPrint.jsx
+++ src/modules/modules/suscriptions/pages/ContractPrint.jsx
@@ -5,6 +5,11 @@ import { Header } from "../../components"
 import { useContractDetailsStore } from "../../../../store"
 import { useGetReports } from "../helpers/useGetReports"
 import { useEffect } from "react"
+import { Viewer, Worker } from "@react-pdf-viewer/core"
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 export const ContractPrint = () => {
 
@@ -32,11 +37,22 @@ export const ContractPrint = () => {
   }, [contractPrint])
 
 
-  const pdfUrl = 'http://154.12.236.19:128/media/contract/contrato_pdf/CON000000002_1.pdf';
-
+  /* const pdfUrl = 'http://154.12.236.19:128/media/contract/contrato_pdf/CON000000002_1.pdf'; */
+  const pdfUrl = 'http://mangel-pc.local:8001/proxy/pdf?url=http://154.12.236.19:128/media/contract/contrato_pdf/CON000000002_1.pdf';
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <Box className="animate__animated animate__fadeIn">
       <Header title={headerTitle} subtitle="Visualiza el contrato y verifica que todo este en orden." />
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Viewer fileUrl={pdfUrl}
+
+          plugins={[
+            // Register plugins
+            defaultLayoutPluginInstance]}
+
+        />
+
+      </Worker>
     </Box>
   )
 }
