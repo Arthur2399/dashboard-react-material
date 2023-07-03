a0c9e881fa8492d5f11417e3c65edd3317bad592
Arthur2399 <arthurchavez2399@gmail.com>
2023-07-03 10:40:49 -0500

    [ADD] Rutas de contabilidad

--- src/modules/modules/accounting/routes/AccountingRoutes.jsx
+++ src/modules/modules/accounting/routes/AccountingRoutes.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-
 import { Accounting,Treasury } from '../pages';
 
 export const AccountingRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Accounting/>} />
       <Route path="tesoreria" element={<Treasury />} />
+      <Route path="estructuraPlanContable" element={<Treasury />} />
+      <Route path="planContable" element={<Treasury />} />
+      <Route path="cuentasCierre" element={<Treasury />} />
+      <Route path="conciliacionBancaria" element={<Treasury />} />
+      <Route path="tiposComprobantes" element={<Treasury />} />
+
+      {/* Movimientos */}
+      <Route path="movimientos/asientosContables" element={<Treasury />} />
+      <Route path="movimientos/comprobantesIngreso" element={<Treasury />} />
+
+      {/* Procesos */}
+      <Route path="procesos/contabilizarCierreTienda" element={<Treasury />} />
+      <Route path="procesos/desaprobarMovimientosContables" element={<Treasury />} />
+      <Route path="procesos/contabilizacionRolPagos" element={<Treasury />} />
+      <Route path="procesos/desaprobarComprobantePago" element={<Treasury />} />
+      <Route path="procesos/contabilizarRetencionesEMP" element={<Treasury />} />
+      <Route path="procesos/contabilizacionAportesPatronales" element={<Treasury />} />
+      <Route path="procesos/pasarDatosIngreso" element={<Treasury />} />
+      <Route path="procesos/RecalculoSaldos" element={<Treasury />} />
+      <Route path="procesos/RecalculoSaldos" element={<Treasury />} />
+      <Route path="procesos/reversarAsientos" element={<Treasury />} />
+      <Route path="procesos/recalcularCuentasPorPagar" element={<Treasury />} />
+      <Route path="procesos/cierreAnual" element={<Treasury />} />
+
+      {/* Reportes */}
+      <Route path="reportes/libroMayorPorRaangoCuentas" element={<Treasury />} />
+      <Route path="reportes/listadoPagos" element={<Treasury />} />
+      <Route path="reportes/saldosContables" element={<Treasury />} />
+      <Route path="reportes/libroDiario" element={<Treasury />} />
+      <Route path="reportes/balanceComprobacion" element={<Treasury />} />
+      <Route path="reportes/estadoSituacionFinanciera" element={<Treasury />} />
+      <Route path="reportes/estadoPerdidadGanancias" element={<Treasury />} />
+      <Route path="reportes/flujoEfectivo" element={<Treasury />} />
+      <Route path="reportes/libroMayorAuxiliar" element={<Treasury />} />
+      <Route path="reportes/libroMayor" element={<Treasury />} />
+      <Route path="reportes/impuestoRenta" element={<Treasury />} />
+
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   )
